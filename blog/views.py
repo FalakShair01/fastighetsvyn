d3f198c5622c8f48104a035f18197c6810b2ab20
FalakShair01 <falakshair.563@gmail.com>
2023-10-19 13:45:52 -0700

    initial commit

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.parsers import MultiPartParser,FormParser
 from .serializers import BlogSerializer
 from .models import Blog
+
 
 # Create your views here.
 
@@ -12,6 +14,8 @@ class BlogView(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticatedOrReadOnly] 
+    parser_classes = [MultiPartParser, FormParser]
+
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)  
