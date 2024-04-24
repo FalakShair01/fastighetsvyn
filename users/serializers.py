085b43367485ecc1c52f979307d5d01cfd53fc98
FalakShair01 <falakshair.563@gmail.com>
2024-04-25 00:02:53 +0500

    added profile image in user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -16,7 +16,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "username", "phone", "address", "password", "role", "is_active", "created_at"]
+        fields = ["id", "email", "username", "profile", "phone", "address", "password", "role", "is_active", "created_at"]
         extra_kwargs = {
             'password': {'write_only': True}
         }
