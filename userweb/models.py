954b01f69d6874207aada42b20df91a24846374c
FalakShair01 <falakshair.563@gmail.com>
2024-08-16 18:55:13 +0500

    added button field in serializer

--- userweb/models.py
+++ userweb/models.py
@@ -50,5 +50,6 @@ class FormLinks(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     link = models.TextField()
+    button_text = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
