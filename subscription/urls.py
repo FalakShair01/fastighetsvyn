6c9ef23d64809f72edd9bd5ee10c254ec2350366
Falak Shair <falak.shair@issm.ai>
2024-09-15 18:30:59 +0500

    added  product list and checkout url

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import stripe_webhook, CheckSubscriptionStatus, CreateCheckoutSessionView
+from .views import stripe_webhook, CheckSubscriptionStatus, CreateCheckoutSessionView, ListPricesView
 
 urlpatterns = [
     path('stripe-webhook/', stripe_webhook, name='stripe-webhook'),
     path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('product/list-prices/', ListPricesView.as_view(), name='list-prices'),
     path('subscription/status/', CheckSubscriptionStatus.as_view())
 ]
