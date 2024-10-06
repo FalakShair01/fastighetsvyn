4e13bdae19b29d86a19b738a722e22dcf8108afc
Falak Shair <falak.shair@issm.ai>
2024-10-07 03:17:14 +0500

    fix proprty filters in tenants

--- users/filters.py
+++ users/filters.py
@@ -1,16 +1,16 @@
 from django_filters import rest_framework as filters
 from .models import Tenant
 
-
 class TenantFilter(filters.FilterSet):
     email = filters.CharFilter(method='filter_by_emails')
+    property = filters.CharFilter(method='filter_by_properties')
 
     class Meta:
         model = Tenant
         fields = {
             "name": ["in", "exact"],
-            "property": ["in", "exact"],
-            "email": ["exact"], 
+            "property": ["exact"],  # Use exact for single property filtering.
+            "email": ["exact"],
             "phone": ["in", "exact"],
             "comment": ["in", "exact"],
         }
@@ -18,3 +18,7 @@ class TenantFilter(filters.FilterSet):
     def filter_by_emails(self, queryset, name, value):
         emails = value.split(',')
         return queryset.filter(email__in=emails)
+
+    def filter_by_properties(self, queryset, name, value):
+        properties = value.split(',')
+        return queryset.filter(property__id__in=properties)
