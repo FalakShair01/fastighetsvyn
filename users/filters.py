2285458c58549de1eddf471538fe73bebe4e4cda
Falak Shair <falak.shair@issm.ai>
2024-10-07 03:09:52 +0500

    filters in tenants issue fix

--- users/filters.py
+++ users/filters.py
@@ -3,12 +3,18 @@ from .models import Tenant
 
 
 class TenantFilter(filters.FilterSet):
+    email = filters.CharFilter(method='filter_by_emails')
+
     class Meta:
         model = Tenant
         fields = {
             "name": ["in", "exact"],
             "property": ["in", "exact"],
-            "email": ["in", "exact"],
+            "email": ["exact"], 
             "phone": ["in", "exact"],
             "comment": ["in", "exact"],
         }
+
+    def filter_by_emails(self, queryset, name, value):
+        emails = value.split(',')
+        return queryset.filter(email__in=emails)
