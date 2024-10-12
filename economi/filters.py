74a55335fbba287775cf71af742683f5dbce4d32
Falak Shair <falak.shair@issm.ai>
2024-10-13 04:31:02 +0500

    multiple filters select in economi

--- economi/filters.py
+++ economi/filters.py
@@ -2,15 +2,18 @@ from django_filters import rest_framework as filters
 from .models import Expense
 from property.models import Property
 
+class MultipleValueFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
 class ExpenseFilter(filters.FilterSet):
     start_date = filters.DateFilter(field_name="date_of_transaction", lookup_expr='gte')
     end_date = filters.DateFilter(field_name="date_of_transaction", lookup_expr='lte')
     type_of_transaction = filters.CharFilter(field_name='type_of_transaction', lookup_expr='icontains')
     type_of_cost_or_revenue = filters.CharFilter(field_name='type_of_cost_or_revenue', lookup_expr='icontains')
-    account = filters.CharFilter(field_name='account', lookup_expr='icontains')
+    account = MultipleValueFilter(field_name='account')  # Allows multiple values for account
     building = filters.ModelMultipleChoiceFilter(queryset=Property.objects.all(), to_field_name='id')  # Allow multiple buildings
-    total_sum = filters.NumericRangeFilter(field_name='total_sum')  # Allows range queries for total_sum
-    value_added_tax = filters.NumericRangeFilter(field_name='value_added_tax')  # Allows range queries for value_added_tax
+    total_sum = filters.BaseInFilter(field_name='total_sum')  # Allows filtering by multiple total_sum values
+    value_added_tax = filters.BaseInFilter(field_name='value_added_tax')  # Allows filtering by multiple VAT values
     comment = filters.CharFilter(field_name='comment', lookup_expr='icontains')
 
     class Meta:
@@ -26,5 +29,4 @@ class ExpenseFilter(filters.FilterSet):
             'value_added_tax',
             'comment',
         ]
-
 
