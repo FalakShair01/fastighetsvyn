b8a4b6f6c85679878f83b423b74d4a86d9ed5ee0
Falak Shair <falak.shair@issm.ai>
2024-10-13 13:45:44 +0500

    resolve filter economi

--- economi/filters.py
+++ economi/filters.py
@@ -2,15 +2,13 @@ from django_filters import rest_framework as filters
 from .models import Expense
 from property.models import Property
 
-class MultipleValueFilter(filters.BaseInFilter, filters.CharFilter):
-    pass
 
 class ExpenseFilter(filters.FilterSet):
     start_date = filters.DateFilter(field_name="date_of_transaction", lookup_expr='gte')
     end_date = filters.DateFilter(field_name="date_of_transaction", lookup_expr='lte')
     type_of_transaction = filters.CharFilter(field_name='type_of_transaction', lookup_expr='icontains')
     type_of_cost_or_revenue = filters.CharFilter(field_name='type_of_cost_or_revenue', lookup_expr='icontains')
-    account = MultipleValueFilter(field_name='account')  # Allows multiple values for account
+    account = filters.CharFilter(field_name='account', lookup_expr='in')  # Use __in lookup for multiple accounts
     building = filters.ModelMultipleChoiceFilter(queryset=Property.objects.all(), to_field_name='id')  # Allow multiple buildings
     total_sum = filters.BaseInFilter(field_name='total_sum')  # Allows filtering by multiple total_sum values
     value_added_tax = filters.BaseInFilter(field_name='value_added_tax')  # Allows filtering by multiple VAT values
