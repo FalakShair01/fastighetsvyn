202bf5e07fb604e05887ddf1e07a13dab1a41e69
Falak Shair <falak.shair@issm.ai>
2024-11-23 17:53:29 +0500

    remove api prefix

--- subscription/urls.py
+++ subscription/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
     path('product/list-prices/', ListPricesView.as_view(), name='list-prices'),
     path('subscription/status/', CheckSubscriptionStatus.as_view()),
-    path('api/extend-subscription/', ExtendSubscriptionView.as_view(), name='extend_subscription'),
+    path('extend-subscription/', ExtendSubscriptionView.as_view(), name='extend_subscription'),
 ]
