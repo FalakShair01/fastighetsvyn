960ed1f7c210240f7e4ea04784bd6e397fb1a9cd
Falak Shair <falak.shair@issm.ai>
2024-12-25 10:19:24 +0500

    added allowed hosts

--- fastighetsvyn/settings.py
+++ fastighetsvyn/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    'fastighetsvyn.vercel.app',
+    'fastighetsvyn.se',
+    'fastvyn.se',
+    # 'localhost',
+    # '127.0.0.1',
+]
 
 
 # Application definition
